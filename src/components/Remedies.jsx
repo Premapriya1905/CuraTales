711705c852ea0c65acf88c9b8236b336ca1d7317
Premapriya <premapriya1905@gmail.com>
2025-04-17 11:32:43 +0530

    scratch10

--- src/components/Remedies.jsx
+++ src/components/Remedies.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "../components/navbar";
+import Navbar from "./Navbar";
 import RemedyModal from "./RemedyModal";
 import remediesData from "../Data/remediesData";
 
